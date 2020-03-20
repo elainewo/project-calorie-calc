54577577ad84ba44b30b2295324df663eae429a7
elaine <yw.4contact@gmail.com>
2020-03-20 09:22:42 +0100

    ProjectCalorieCalc v.0.0.3

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,70 +9,14 @@ function rechnen() {
     if (document.getElementsByName("radio")[0].checked) {
         console.log("Weiblich")
         let Grundumsatz = 655.1 + (9.6 * gewicht) + (1.8 * grosse) - (4.7 * alter);
-        if (y == 0) {
-            console.log("schlafen");
-            let Gesamtumsatz = Grundumsatz * 0.95;
-            document.getElementById("kcal2").innerHTML = `Dein Gesamtumsatz ist ${Gesamtumsatz.toFixed(1)} kcal`;
-        }
-        if (y == 1) {
-            console.log("sitzen");
-            let Gesamtumsatz = Grundumsatz * 1.2;
-            document.getElementById("kcal2").innerHTML = `Dein Gesamtumsatz ist ${Gesamtumsatz.toFixed(1)} kcal`;
-        }
-        if (y == 2) {
-            console.log("büro");
-            let Gesamtumsatz = Grundumsatz * 1.5;
-            document.getElementById("kcal2").innerHTML = `Dein Gesamtumsatz ist ${Gesamtumsatz.toFixed(1)} kcal`;
-        }
-        if (y == 3) {
-            console.log("student");
-            let Gesamtumsatz = Grundumsatz * 1.7;
-            document.getElementById("kcal2").innerHTML = `Dein Gesamtumsatz ist ${Gesamtumsatz.toFixed(1)} kcal`;
-        }
-        if (y == 4) {
-            console.log("verkäufer");
-            let Gesamtumsatz = Grundumsatz * 1.9;
-            document.getElementById("kcal2").innerHTML = `Dein Gesamtumsatz ist ${Gesamtumsatz.toFixed(1)} kcal`;
-        }
-        if (y == 5) {
-            console.log("arbeiter");
-            let Gesamtumsatz = Grundumsatz * 2.2;
-            document.getElementById("kcal2").innerHTML = `Dein Gesamtumsatz ist ${Gesamtumsatz.toFixed(1)} kcal`;
-        }
+        let Gesamtumsatz = Grundumsatz * y;
+        document.getElementById("kcal2").innerHTML = `Dein Gesamtumsatz ist ${Gesamtumsatz.toFixed(1)} kcal`;
         document.getElementById("kcal").innerHTML = `Dein Grundumsatz ist ${Grundumsatz.toFixed(1)} kcal`;
     } else {
         console.log("Männlich")
         let Grundumsatz2 = 664.7 + (13.7 * gewicht) + (5 * grosse) - (6.8 * alter);
-        if (y == 0) {
-            console.log("schlafen");
-            let Gesamtumsatz2 = Grundumsatz2 * 0.95;
-            document.getElementById("kcal2").innerHTML = `Dein Gesamtumsatz ist ${Gesamtumsatz2.toFixed(1)} kcal`;
-        }
-        if (y == 1) {
-            console.log("sitzen");
-            let Gesamtumsatz2 = Grundumsatz2 * 1.2;
-            document.getElementById("kcal2").innerHTML = `Dein Gesamtumsatz ist ${Gesamtumsatz2.toFixed(1)} kcal`;
-        }
-        if (y == 2) {
-            console.log("sitzen");
-            let Gesamtumsatz2 = Grundumsatz2 * 1.5;
-            document.getElementById("kcal2").innerHTML = `Dein Gesamtumsatz ist ${Gesamtumsatz2.toFixed(1)} kcal`;
-        }
-        if (y == 3) {
-            console.log("student");
-            let Gesamtumsatz2 = Grundumsatz2 * 1.7;
-            document.getElementById("kcal2").innerHTML = `Dein Gesamtumsatz ist ${Gesamtumsatz2.toFixed(1)} kcal`;
-        }
-        if (y == 4) {
-            console.log("verkäufer");
-            let Gesamtumsatz2 = Grundumsatz2 * 1.9;
-            document.getElementById("kcal2").innerHTML = `Dein Gesamtumsatz ist ${Gesamtumsatz2.toFixed(1)} kcal`;
-        }
-        if (y == 5) {
-            console.log("arbeiter");
-            let Gesamtumsatz2 = Grundumsatz2 * 2.2;
-            document.getElementById("kcal2").innerHTML = `Dein Gesamtumsatz ist ${Gesamtumsatz2.toFixed(1)} kcal`;
-        }
+        let Gesamtumsatz2 = Grundumsatz2 * y;
+        document.getElementById("kcal2").innerHTML = `Dein Gesamtumsatz ist ${Gesamtumsatz2.toFixed(1)} kcal`;
         document.getElementById("kcal").innerHTML = `Dein Grundumsatz ist ${Grundumsatz2.toFixed(1)} kcal`;
     }
 }
