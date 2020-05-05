f3813b845a4c0352d4e59dced9f4073496290d3f
elainewo <yw.4contact@gmail.com>
2020-05-05 16:16:29 +0200

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,32 @@
-console.log("test")
+let gewicht = document.getElementById("gewicht");
+let größe = document.getElementById("größe");
+let alter = document.getElementById("alter");
+let aktivität = document.getElementById("aktivität");
+let grundumsatz;
+let index;
 
-function rechnen() {
-    let grosse = document.getElementById("grosse").value;
-    let alter = document.getElementById("alter").value;
-    let gewicht = document.getElementById("gewicht").value;
-    let y = getFreizeit();
+document.getElementById("button").addEventListener("click", () => {
+  let i = aktivität.selectedIndex;
+  console.log(i);
+  index = aktivität.options[i].value;
 
-    if (document.getElementsByName("radio")[0].checked) {
-        console.log("Weiblich")
-        let Grundumsatz = 655.1 + (9.6 * gewicht) + (1.8 * grosse) - (4.7 * alter);
-        let Gesamtumsatz = Grundumsatz * y;
-        document.getElementById("kcal2").innerHTML = `Dein Gesamtumsatz ist ${Gesamtumsatz.toFixed(1)} kcal`;
-        document.getElementById("kcal").innerHTML = `Dein Grundumsatz ist ${Grundumsatz.toFixed(1)} kcal`;
-    } else {
-        console.log("Männlich")
-        let Grundumsatz2 = 664.7 + (13.7 * gewicht) + (5 * grosse) - (6.8 * alter);
-        let Gesamtumsatz2 = Grundumsatz2 * y;
-        document.getElementById("kcal2").innerHTML = `Dein Gesamtumsatz ist ${Gesamtumsatz2.toFixed(1)} kcal`;
-        document.getElementById("kcal").innerHTML = `Dein Grundumsatz ist ${Grundumsatz2.toFixed(1)} kcal`;
-    }
-}
-
-let getFreizeit = () => {
-    let j = document.getElementById("freizeit").selectedIndex
-    let auswahl = document.getElementById("freizeit").options[j].value
-    console.log(auswahl)
-    return auswahl
-}
+  if (document.getElementById("weiblich").checked) {
+    grundumsatz =
+      655.1 + 9.6 * gewicht.value + 1.8 * größe.value - 4.7 * alter.value;
+  } else {
+    grundumsatz =
+      66.47 + 13.7 * gewicht.value + 5 * größe.value - 6.8 * alter.value;
+  }
+  document.getElementsByTagName("td")[1].innerHTML = grundumsatz.toFixed(0);
+  document.getElementsByTagName("td")[2].innerHTML = (
+    grundumsatz * 4.184
+  ).toFixed(0);
+  document.getElementsByTagName("td")[4].innerHTML = (
+    grundumsatz * index
+  ).toFixed(0);
+  document.getElementsByTagName("td")[5].innerHTML = (
+    grundumsatz *
+    4.184 *
+    index
+  ).toFixed(0);
+});
